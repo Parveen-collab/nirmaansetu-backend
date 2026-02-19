08134ccc875fb67ab2440ec127dfed8c1d4fc761
Parveen_collab <pk16061999@gmail.com>
2026-02-19 15:26:54 +0530

    first commit

---
+++ backend/src/test/java/com/nirmaansetu/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirmaansetu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
